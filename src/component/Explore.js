926e309e568e2750773f2c4439d72bc670fb786b
Satish Narayan <narayansatish97@gmail.com>
2020-12-29 14:35:58 +0530

    explore page ready

--- src/component/Explore.js
+++ src/component/Explore.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Navbar, NavItem, NavDropdown, MenuItem, Nav, Row,Col,Container,Button } from 'react-bootstrap';
 
@@ -13,19 +13,39 @@ import Topic from "./Topic.js";
 //
 function Explore()
     {    let topic =["All", 'javascript', 'react', 'python', 'golang'];
-        console.log(topic);
-         let topic_html=topic.map((item)=><Col lg={{ span:1}} xs={2}> <Button variant="Dark"><Link to={"/Explore/"+item} >{item}</Link></Button></Col>);
-         console.log(topic_html);
-         let switchone=topic.map((item)=><Route path={"/Explore/"+item} component={(item)=><Topic language={item}/> } exact />);
-         console.log(switchone); 
+
+         let  [lang, setlang] = useState(null);
+
+         let [loading,setloading]=useState(0);
+
+         let topic_html=topic.map((item)=><Col lg={{ span:1}} xs={2}> <Button variant="Dark"   onClick={(item)=>{setlang(item);setloading(1);}}>{item}</Button></Col>);
+         
+         let  [items, setitems] = useState(null);
+           
+            useEffect(()=> {    if(lang!=null) 
+                                   { 
+                                    let lang_api = "https://api.github.com/search/repositories?q=stars:%3E1+language:"+lang+"&sort=stars&order=desc&type=Repositories";
+                                    fetch(lang_api).then(response => response.json()).then(data =>{
+                                                                                                let items=data["items"];
+                
+                                                                                                items.sort((a,b)=>a["stargazers_count"]<b["stargazers_count"]);
+
+                                                                                                let top_30=items.map((item)=><div><img src={item["owner"]["avatar_url"]} /><h3>{item["full_name"]}</h3><h4>{item["stargazers_count"]}</h4></div>);
+                                                                                                setitems(top_30);
+                                                                                                  setloading(0);
+                                                                                                });
+                                    }
+                            } 
+                        ,[lang]);
+         
         return (<div>
                     <Row>
                         {topic_html}
                     </Row>
                     <main>
-                        <Switch>
-                            {switchone}
-                        </Switch>
+                    {loading?<h1>loading</h1>:""}
+                    {lang!=null ? items: ''}  
+
                     </main>
                 </div>
 
